b3c3ad3514cc56448739d22e9b31db34df9d8ce5
khaled elaadwy <k.elaadwy@gmail.com>
2022-05-29 14:19:19 +0200

    add first person movements

--- PharusEvents/Assets/Scripts/Player/PlayerController.cs
+++ PharusEvents/Assets/Scripts/Player/PlayerController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
+
+    [SerializeField] GameObject cameraHolder;
+
     [SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
     float verticalLookRotation;
-    bool grounded;
+    bool isGrounded;
     Vector3 smoothMoveVelocity;
-    Vector3 noveAmount;
+    Vector3 moveAmount;
 
     void Awake()
     {
@@ -18,6 +21,45 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+        Look();
+        Move();
+        Jump();
     }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+    }
+        
+    void Look()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+
+        verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+    }
+
+    void Move()
+    {
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed),
+            ref smoothMoveVelocity, smoothTime);
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+
+    public void SetGroundedState(bool _grounded)
+    {
+        isGrounded = _grounded;
+    }
+    
 }
